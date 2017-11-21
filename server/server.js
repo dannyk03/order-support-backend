a2edf313a8513763edc1566be471406a93bfac95
itsolution-git <roy.smith0820@gmail.com>
2017-11-21 14:49:55 -0500

    google auth backend module done

--- server/server.js
+++ server/server.js
@@ -2,8 +2,124 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
 var app = module.exports = loopback();
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var loopbackPassport = require('loopback-component-passport');
+var PassportConfigurator = loopbackPassport.PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+var flash = require('express-flash');
+var jwt = require('json-web-token');
+
+var secretKey = "wj6aQMAclS"
+
+var config = {};
+try {
+  // config = require('./providers.dev.json');
+  config = require('./providers.json');
+} catch (err) {
+  console.trace(err);
+  process.exit(1); // fatal
+}
+// boot scripts mount components like REST API
+boot(app, __dirname);
+
+// to support JSON-encoded bodies
+app.middleware('parse', bodyParser.json());
+// to support URL-encoded bodies
+app.middleware('parse', bodyParser.urlencoded({
+  extended: true,
+}));
+
+app.middleware('auth', loopback.token({
+  model: app.models.accessToken,
+}));
+
+app.middleware('session:before', cookieParser(app.get('cookieSecret')));
+app.middleware('session', session({
+  secret: 'kitty',
+  saveUninitialized: true,
+  resave: true,
+}));
+passportConfigurator.init();
+
+// We need flash messages to see passport errors
+app.use(flash());
+
+passportConfigurator.setupModels({
+  userModel: app.models.user,
+  userIdentityModel: app.models.userIdentity,
+  userCredentialModel: app.models.userCredential,
+});
+for (var s in config) {
+  var c = config[s];
+  c.session = c.session !== false;
+  passportConfigurator.configureProvider(s, c);
+}
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
+
+app.get('/auth/account', ensureLoggedIn('/login'), function (req, res, next) {
+  let payload = {
+    name: req.user.profiles[0].profile.displayName,
+    email: req.user.profiles[0].profile.emails[0].value,
+  }
+  jwt.encode(secretKey, payload, function (err, token) {
+    res.redirect('http://localhost:4000/auth/login_success/' + token)
+  })
+});
+
+app.get('/auth/google', ensureLoggedIn('/login'), function (req, res, next) {
+  res.send({
+    user: req.user,
+    url: req.url,
+  });
+});
+
+app.get('/login', function (req, res, next) {
+  res.send({
+    user: req.user,
+    url: req.url,
+  });
+});
+
+
+app.post('/signup', function (req, res, next) {
+  var User = app.models.user;
+
+  var newUser = {};
+  newUser.email = req.body.email.toLowerCase();
+  newUser.username = req.body.username.trim();
+  newUser.password = req.body.password;
+
+  User.create(newUser, function (err, user) {
+    if (err) {
+      req.flash('error', err.message);
+      return res.redirect('back');
+    } else {
+      // Passport exposes a login() function on req (also aliased as logIn())
+      // that can be used to establish a login session. This function is
+      // primarily used when users sign up, during which req.login() can
+      // be invoked to log in the newly registered user.
+      req.login(user, function (err) {
+        if (err) {
+          req.flash('error', err.message);
+          return res.redirect('back');
+        }
+        return res.redirect('/auth/account');
+      });
+    }
+  });
+});
+
+app.get('/auth/logout', function (req, res, next) {
+  req.logout();
+  // res.redirect('/');
+  res.send({
+    message: 'user logged out successfully'
+  })
+});
+
 
 app.start = function() {
   // start the web server
@@ -20,10 +136,7 @@ app.start = function() {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
-
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
-});
+// start the server if `$ node server.js`
+if (require.main === module) {
+  app.start();
+}
