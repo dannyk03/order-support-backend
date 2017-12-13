8ad55a7a41428418079ed8632dc81743f57392c6
itsolution-git <roy.smith0820@gmail.com>
2017-12-13 03:07:07 -0500

    fixed spell error

--- common/models/fileobject.js
+++ common/models/fileobject.js
@@ -3,7 +3,7 @@
 module.exports = function(fileobject) {
 	fileobject.afterRemote('create', function(context, file_object, next){
 		fileobject.app.models.customer.findById(file_object.customer_id, {}, function(err, customer_data){
-			file_object["cusotmer"] = customer_data
+			file_object["customer"] = customer_data
 			context.result = file_object
 			next()
 		})
