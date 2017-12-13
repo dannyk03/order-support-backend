bf0bf7deb85135339f2a4e39c4caf13986522298
itsolution-git <roy.smith0820@gmail.com>
2017-12-13 02:58:49 -0500

    update fileobject to get customer data from id

--- common/models/fileobject.js
+++ common/models/fileobject.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = function(fileobject) {
-
+	fileobject.afterRemote('create', function(context, file_object, next){
+		fileobject.app.models.customer.findById(file_object.customer_id, {}, function(err, customer_data){
+			file_object["cusotmer"] = customer_data
+			context.result = file_object
+			next()
+		})
+	})
 };
